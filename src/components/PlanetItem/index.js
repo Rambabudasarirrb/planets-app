3659ef3fa206f35fa9c2e70895d9e44518b23ebe
Rambabu <rambabu@example.com>
2022-11-03 13:43:10 +0000

    Planets App using react js

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="list-items">
+      <img src={imageUrl} alt={`planet ${name}`} className="image" />
+      <h1 className="heading">{name}</h1>
+      <p className="description">{description}</p>
+    </div>
+  )
+}
+export default PlanetItem
